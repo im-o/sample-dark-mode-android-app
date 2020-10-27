01f1c30dd62261477ad662f0375abe3ae4420eb0
im-o <frizgt@gmail.com>
2020-10-28 00:14:24 +0800

    :zap: add function apply dark mode

--- app/src/main/java/com/stimednp/darkmodeapp/MainActivity.kt
+++ app/src/main/java/com/stimednp/darkmodeapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.stimednp.darkmodeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        applyModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            when(isChecked){
+                true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                false -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
